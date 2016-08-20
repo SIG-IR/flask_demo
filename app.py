0e38d43daaf0e969ea84d363dca8be1a2210af7c
Sameet Sapra <sameetandpotatoes@gmail.com>
2016-08-20 18:06:36 -0500

    Add Tweepy API integration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import tweepy
+from twitter_authentication import API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 import json
 
 app = Flask(__name__)
@@ -7,11 +9,22 @@ app = Flask(__name__)
 def index():
     return "Hello World"
     
-@app.route("/json")
-def json():
+@app.route("/json_example")
+def json_example():
     return json.dumps({
         'key': 'value'
     })
+
+@app.route("/twitter")
+def twitter():
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    query = 'Donald Trump'
+    max_tweets = 2
+    searched_tweets = api.search(q=query, count=max_tweets)
+    return json.dumps(searched_tweets)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
